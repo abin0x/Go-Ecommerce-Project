0d823800c03b8c1988e131901b2f3875c63e5fea
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-29 20:21:48 +0600

    middleware adding

--- cmd/serve.go
+++ cmd/serve.go
@@ -3,15 +3,19 @@ package cmd
 import (
 	"ecommerce/global_router"
 	"ecommerce/handlers"
+	"ecommerce/middleware"
 	"fmt"
 	"net/http"
 )
 
 func Serve() {
 	mux := http.NewServeMux()
-	mux.Handle("GET /products", http.HandlerFunc(handlers.GetProductsHandler))
-	mux.Handle("POST /products", http.HandlerFunc(handlers.CreateProductHandler))
-	mux.Handle("GET /products/{id}", http.HandlerFunc(handlers.GetProductById))
+
+	mux.Handle("GET /abin", middleware.Logger(http.HandlerFunc(handlers.Test)))
+
+	mux.Handle("GET /products", middleware.Logger(http.HandlerFunc(handlers.GetProductsHandler)))
+	mux.Handle("POST /products", middleware.Logger(http.HandlerFunc(handlers.CreateProductHandler)))
+	mux.Handle("GET /products/{id}", middleware.Logger(http.HandlerFunc(handlers.GetProductById)))
 
 	fmt.Println("Starting server on :8080")
 	globalRouter := global_router.GlobalRouter(mux)
