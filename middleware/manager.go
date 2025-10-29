a682bb0a1138a240f929d9d0b58feb8bc2716af1
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-29 21:13:40 +0600

    somting bujtechi

--- middleware/manager.go
+++ middleware/manager.go
@@ -15,14 +15,10 @@ func NewManager() *Manager {
 
 }
 
-func (mngr *Manager) With(middlewares ...Middleware) Middleware {
-	return func(next http.Handler) http.Handler {
-		n := next
-		for i := len(middlewares) - 1; i >= 0; i-- {
-			middleware := middlewares[i]
-			n = middleware(n)
-		}
-		return n
+func (mngr *Manager) With(next http.Handler, middlewares ...Middleware) http.Handler {
+	n := next
+	for _, middleware := range middlewares {
+		n = middleware(n)
 	}
-
+	return n
 }
