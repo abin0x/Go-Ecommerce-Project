338dbb38a07c97dc0077ae90b46ac4232dec9a56
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-29 20:30:56 +0600

    middleware

--- cmd/serve.go
+++ cmd/serve.go
@@ -11,11 +11,11 @@ import (
 func Serve() {
 	mux := http.NewServeMux()
 
-	mux.Handle("GET /abin", middleware.Logger(http.HandlerFunc(handlers.Test)))
+	mux.Handle("GET /abin", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.Test))))
 
-	mux.Handle("GET /products", middleware.Logger(http.HandlerFunc(handlers.GetProductsHandler)))
-	mux.Handle("POST /products", middleware.Logger(http.HandlerFunc(handlers.CreateProductHandler)))
-	mux.Handle("GET /products/{id}", middleware.Logger(http.HandlerFunc(handlers.GetProductById)))
+	mux.Handle("GET /products", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.GetProductsHandler))))
+	mux.Handle("POST /products", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.CreateProductHandler))))
+	mux.Handle("GET /products/{id}", middleware.Hudai(middleware.Logger(http.HandlerFunc(handlers.GetProductById))))
 
 	fmt.Println("Starting server on :8080")
 	globalRouter := global_router.GlobalRouter(mux)
