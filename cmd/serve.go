1d1e73c24c9f169b90b2b5430bbd960ee0c5f7ee
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-10-30 10:35:44 +0600

    gfdskd

--- cmd/serve.go
+++ cmd/serve.go
@@ -8,13 +8,17 @@ import (
 
 func Serve() {
 	manager := middleware.NewManager()
-	manager.Use(middleware.Logger, middleware.Hudai, middleware.CorsWithPreflight)
 
 	mux := http.NewServeMux()
 	InitRoutes(mux, manager)
 
 	// globalRouter := middleware.CorsWithPreflight(mux)
-	wrappedMux := manager.WrapMux(mux)
+	wrappedMux := manager.WrapMux(
+		mux,
+		middleware.Logger,
+		middleware.Hudai,
+		middleware.CorsWithPreflight,
+	)
 	fmt.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", wrappedMux)
 	if err != nil {
