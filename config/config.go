a34e33099eded70dcd78b1ed84f0b69fbb6cdb42
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-02 10:19:37 +0600

    load config update

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var configurations Config
+var configurations *Config
 
 type Config struct {
 	Version     string
@@ -54,7 +54,7 @@ func loadConfig() {
 		os.Exit(1)
 	}
 
-	configurations = Config{
+	configurations = &Config{
 		Version:     version,
 		ServiceName: serviceName,
 		HttpPort:    int(port),
@@ -64,6 +64,9 @@ func loadConfig() {
 }
 
 func GetConfig() *Config {
-	loadConfig()
-	return &configurations
+
+	if configurations == nil {
+		loadConfig()
+	}
+	return configurations
 }
