f5745b04cd4ad124025284ed7fccbd7f692b9c20
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-02 09:43:17 +0600

    done

--- cmd/serve.go
+++ cmd/serve.go
@@ -3,9 +3,15 @@ package cmd
 import (
 	"ecommerce/config"
 	"ecommerce/rest"
+	"ecommerce/rest/handlers/product"
+	"ecommerce/rest/handlers/user"
 )
 
 func Serve() {
 	cnf := config.GetConfig()
-	rest.Start(cnf)
+
+	productHandler := product.NewHandler()
+	userHandler := user.NewHandler()
+	server := rest.NewServer(productHandler, userHandler)
+	server.Start(cnf)
 }
