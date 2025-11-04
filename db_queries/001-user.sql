415adedbd5150d31027cc19fb73df08ece157409
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-04 09:57:51 +0600

    data type

--- db_queries/001-user.sql
+++ db_queries/001-user.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
     first_name   VARCHAR(100) NOT NULL,
     last_name    VARCHAR(100) NOT NULL,
     email        VARCHAR(150) UNIQUE NOT NULL,
+    age INT NOT NULL,
     password     TEXT NOT NULL,
     is_shop_owner BOOLEAN DEFAULT FALSE
 );
