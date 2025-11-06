20f20c3f6215bf2efa8c83112598f4cfe9844023
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-06 23:07:41 +0600

    datebase inject

--- repo/product.go
+++ repo/product.go
@@ -1,5 +1,7 @@
 package repo
 
+import "github.com/jmoiron/sqlx"
+
 type Product struct {
 	ID          int     `json:"id"`
 	Title       string  `json:"title"`
@@ -17,13 +19,14 @@ type ProductRepo interface {
 }
 
 type productRepo struct {
-	productList []*Product
+	db *sqlx.DB
 }
 
-func NewProductRepo() ProductRepo {
-	repo := &productRepo{}
-	generateInitialProducts(repo)
-	return repo
+func NewProductRepo(db *sqlx.DB) ProductRepo {
+	return &productRepo{
+		db: db,
+	}
+
 }
 
 func (r *productRepo) Create(p Product) (*Product, error) {
@@ -65,52 +68,4 @@ func (r *productRepo) Delete(productID int) error {
 	}
 	r.productList = tempList
 	return nil
-}
-
-func generateInitialProducts(r *productRepo) {
-	// Initialize the product list with some sample products
-	prd1 := &Product{
-		ID:          1,
-		Title:       "Laptop",
-		Description: "A high-performance laptop",
-		Price:       999.99,
-		ImgUrl:      "https://techterms.com/img/xl/laptop_586.png",
-	}
-	prd2 := &Product{
-		ID:          2,
-		Title:       "Smartphone",
-		Description: "A latest model smartphone",
-		Price:       699.99,
-		ImgUrl:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTYAx3hqK3b3i-JGJlw7nhqqrccJtkMnNv8Q&s",
-	}
-	prd3 := &Product{
-		ID:          3,
-		Title:       "Headphones",
-		Description: "Noise-cancelling headphones",
-		Price:       199.99,
-		ImgUrl:      "https://cdn.mos.cms.futurecdn.net/HMCWShKerkfeNQmYYhE3p7.jpg",
-	}
-	prd4 := &Product{
-		ID:          4,
-		Title:       "Smartwatch",
-		Description: "A smartwatch with various features",
-		Price:       299.99,
-		ImgUrl:      "https://istarmax.com/wp-content/uploads/2024/04/Starmax-Product-Range-Summer-2024-2.png",
-	}
-	prd5 := &Product{
-		ID:          5,
-		Title:       "Tablet",
-		Description: "A lightweight tablet",
-		Price:       399.99,
-		ImgUrl:      "https://p2-ofp.static.pub/fes/cms/2021/10/28/juqs65pgl1gh3dysi7yv1tnvtsiqva364946.png",
-	}
-	prd6 := &Product{
-		ID:          6,
-		Title:       "Camera",
-		Description: "A digital camera",
-		Price:       499.99,
-		ImgUrl:      "https://www.gearpatrol.com/wp-content/uploads/sites/2/2023/09/best-vintage-film-cameras-refresh-lead-650de2dd54d04-jpg.webp",
-	}
-	// productList = []Product{prd1, prd2, prd3, prd4, prd5, prd6}
-	r.productList = append(r.productList, prd1, prd2, prd3, prd4, prd5, prd6)
 }
