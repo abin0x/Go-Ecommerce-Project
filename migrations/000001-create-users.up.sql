63d16eb43033e1c0476b7125224349c2be31e52e
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-07 20:34:34 +0600

    sql migrations

---
+++ migrations/000001-create-users.up.sql
@@ -0,0 +1,13 @@
+-- +migrate Up
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name   VARCHAR(100) NOT NULL,
+    last_name    VARCHAR(100) NOT NULL,
+    email        VARCHAR(150) UNIQUE NOT NULL,
+    age INT NOT NULL,
+    money REAL NOT NULL,
+    description NOT NULL,
+    password     TEXT NOT NULL,
+    is_shop_owner BOOLEAN DEFAULT FALSE
+);
+
