29d972403aed101ba6e882ebdceb0b90c3093ea7
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-07 20:56:37 +0600

    sql migrations

--- migrations/000001-create-users.up.sql
+++ migrations/000001-create-users.up.sql
@@ -4,9 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     first_name   VARCHAR(100) NOT NULL,
     last_name    VARCHAR(100) NOT NULL,
     email        VARCHAR(150) UNIQUE NOT NULL,
-    age INT NOT NULL,
-    money REAL NOT NULL,
-    description NOT NULL,
     password     TEXT NOT NULL,
     is_shop_owner BOOLEAN DEFAULT FALSE
 );
