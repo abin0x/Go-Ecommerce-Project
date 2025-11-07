1a3da64c59e77d00a01fe1aab7876f004acf459a
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-07 20:35:11 +0600

    sql migrations

--- migrations/000001-create-users.up.sql
+++ migrations/000001-create-users.up.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name   VARCHAR(100) NOT NULL,
     last_name    VARCHAR(100) NOT NULL,
