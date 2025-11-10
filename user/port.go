2d1e47c8ef9510f16be11509119af40f25eafdae
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-10 13:20:09 +0600

    Now start Domain Driven Design

--- user/port.go
+++ user/port.go
@@ -1,1 +1,11 @@
 package user
+
+import "ecommerce/domain"
+
+type UserRepo interface {
+	Create(user domain.User) (*domain.User, error)
+	Find(email, pass string) (*domain.User, error)
+	// List() ([]*User, error)
+	// Delete(productID int) error
+	// Update(user User) (*User, error)
+}
