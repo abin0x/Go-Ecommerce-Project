656796e4d627545c53dedd771006bbe422b999a3
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-11-14 10:58:43 +0600

    DDD in product

--- product/port.go
+++ product/port.go
@@ -1,1 +1,11 @@
 package product
+
+import "ecommerce/domain"
+
+type ProductRepo interface {
+	Create(p domain.Product) (*domain.Product, error)
+	Get(productID int) (*domain.Product, error)
+	List() ([]*domain.Product, error)
+	Delete(productID int) error
+	Update(p domain.Product) (*domain.Product, error)
+}
