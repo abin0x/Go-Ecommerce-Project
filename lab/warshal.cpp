f086f863dad968f52d2afbb9d4e5caeeb22e83f4
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-12-01 19:37:44 +0600

    fix: resolve issue in warshal

--- lab/warshal.cpp
+++ lab/warshal.cpp
@@ -30,9 +30,11 @@ int main() {
 
       for (int k = 1; k <= n; k++) {
     for (int i = 1; i <= n; i++) {
-      v cv if (!reach[i][k]) continue;
-      for (int j = 1; j <= c vc n; j++) {
-        cbbxvb if (reach[k][j]) reach[i][j] = 1;
+      if (!reach[i][k])
+        continue;
+      for (int j = 1; j <= n; j++) {
+        if (reach[k][j])
+          reach[i][j] = 1;
       }
     }
   }
