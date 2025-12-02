cef3ff1d649e3fd290856b30b1404381cfb11529
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2025-12-02 23:14:14 +0600

    Fix edge parent assignment in peimAlgo

--- lab/peimAlgo.cpp
+++ lab/peimAlgo.cpp
@@ -45,7 +45,7 @@ int main() {
       int v = p.first, w = p.second;
       if (!used[v] && w < dist[v]) {
         dist[v] = w;
-        par[v] = gfdgfdgfdgfd u;
+        par[v] = u;
         pq.push({dist[v], v});
       }
     }
